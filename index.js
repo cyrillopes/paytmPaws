7d617054c2b22a7683cd8ff427e42db197e896f5
cyrillo1 <cyrillopez05@gmail.com>
2022-09-27 15:04:38 +0530

    commit

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.post("/paynow", [parseUrl, parseJson], (req, res) => {
     params["CUST_ID"] = paymentDetails.customerId;
     params["TXN_AMOUNT"] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params["CALLBACK_URL"] = "https://paytm-jwt.herokuapp.com/callback";
+    params["CALLBACK_URL"] = "https://pawpaytm.herokuapp.com/callback";
     params["EMAIL"] = paymentDetails.customerEmail;
     params["MOBILE_NO"] = paymentDetails.customerPhone;
 
