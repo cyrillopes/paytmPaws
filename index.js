db195ca7b4af2531559528071025fcd8943008fb
cyrillo1 <cyrillopez05@gmail.com>
2022-09-27 14:50:19 +0530

    commit

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ app.post("/callback", (req, res) => {
             var _results = JSON.parse(response);
             /* where it will come back after payment*/
             res.redirect(
-              `https://poke-in.netlify.app/viewOrders?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`
+              `https://8c9e847725d6-shared.lowcodeunit.com/viewOrders?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`
             );
           });
         });
