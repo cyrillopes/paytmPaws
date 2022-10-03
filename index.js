23b48c6add0455b3fdb43cd27724ac9e076ecc1c
cyrillo1 <cyrillopez05@gmail.com>
2022-10-03 16:11:41 +0530

    d

--- index.js
+++ index.js
@@ -140,7 +140,7 @@ app.post("/callback", (req, res) => {
             var _results = JSON.parse(response);
             /* where it will come back after payment*/
             res.redirect(
-              `https://d388dc520556-shared.lowcodeunit.com/viewOrders?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`
+              `https://fc9d6728a786-shared.lowcodeunit.com/viewOrders?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`
             );
           });
         });
